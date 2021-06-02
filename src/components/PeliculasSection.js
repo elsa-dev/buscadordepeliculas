3aac0dc9f631fad7d93da9c1d604dd9fe30e40cd
Elsa-dev <beadearg@hotmail.com>
2021-06-02 18:30:10 -0300

    fetch busqueda

--- src/components/PeliculasSection.js
+++ src/components/PeliculasSection.js
@@ -7,11 +7,13 @@ const PeliculasSection = ({peliculas, titulo}) => {
         <section>
             <h2>{titulo}</h2>
             <div>
-                {
+                { peliculas &&
                     peliculas.map( (pelicula) => (
                         <Link to={`/detalle/${pelicula.id}`}>
                             <article>
-                                {pelicula.title}
+                                <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`} />
+                                <h3>{pelicula.title}
+                                    </h3>
                             </article>
                         </Link>
                     )
--- src/views/Busqueda.js
+++ src/views/Busqueda.js
@@ -1,15 +1,52 @@
+import { useState, useEffect } from "react"
+import PeliculasSection from '../components/PeliculasSection'
+
 
 
 const Busqueda = () => {
-    
+    const [valorDelInput, setValorDelInput] = useState('')
+    const [busqueda, setBusqueda] = useState('')
+    const [peliculaBuscada, setPeliculaBuscada] = useState([])
+
+    useEffect ( ()=> {
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=c7e318bc4679faa16a6f940e1435e019&languaje=es-ES&query=${valorDelInput}&page=1`)
+        .then( res => res.json())
+        .then ( data => setPeliculaBuscada(data.results))
+
+    }, [busqueda])
+
+    console.log(peliculaBuscada)
+
+
+
+    const handleChange = (e) => {
+        setValorDelInput(e.target.value)
+    }
+
+    console.log(busqueda)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setBusqueda(valorDelInput)
+    }
+
     return(
-        <form>
+        <>
+         <form onSubmit={handleSubmit}>
             <label>
                 Busca tu Pelicula
-               <input type="text" value="" /> 
+               <input  value={valorDelInput} onChange={handleChange} /> 
             </label>
             
         </form>
+
+        <PeliculasSection 
+        peliculas={peliculaBuscada}
+        
+        />
+       
+        </>
+       
         
         
     )
@@ -17,4 +54,19 @@ const Busqueda = () => {
 
 export default Busqueda;
 
-//fetch inicial a https://api.themoviedb.org/3/search/movie?api_key=c7e318bc4679faa16a6f940e1435e019&languaje=es-ES&query=undefined&page=1
+//fetch inicial a
+//https://api.themoviedb.org/3/search/movie?api_key=c7e318bc4679faa16a6f940e1435e019&languaje=es-ES&query=undefined&page=1
+//https://api.themoviedb.org/3/search/movie?api_key=c7e318bc4679faa16a6f940e1435e019&languaje=es-ES&query=${valorDelInput}&page=1
+
+
+
+
+//  <section>
+//             {peliculaBuscada && peliculaBuscada.map( pelicula=>
+//                 <div>
+//                     <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`} />
+//                     <h2>{pelicula.original_title}</h2>
+//                 </div>
+//             )}
+
+//         </section>
